073ff3c9056c7d506b6f684cf99bfe6330470300
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2024-09-25 09:35:56 +0500

    feat: add auth check

--- src/common/auth/authStateProvider/withPrivateRoute.tsx
+++ src/common/auth/authStateProvider/withPrivateRoute.tsx
@@ -2,16 +2,23 @@ import { FunctionComponent, useContext, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 import { AuthContext } from './authContext'
+import { auth } from '../auth.helper'
 
 export const withPrivateRoute = <Type extends Record<string, unknown>>(ComposedComponent: FunctionComponent<Type>) => {
   return function RequireAuthentication(props: Type) {
     const {
-      isAuthenticated,
+      isAuthenticated, setIsAuthenticated,
     } = useContext(AuthContext)
 
     const navigation = useNavigate()
 
     const location = useLocation()
+
+    const token = auth.getToken()
+
+    if (token) {
+      setIsAuthenticated(true)
+    }
 
     useEffect(() => {
       if (!isAuthenticated) {
