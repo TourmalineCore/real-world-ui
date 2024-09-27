61320c1c1b12fee446d1abf8554170ac8727ce08
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2024-09-27 09:00:47 +0500

    chore: change config builder

--- local-config-builder.js
+++ local-config-builder.js
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/quotes */
-// eslint-disable-next-line no-undef
+import fs from "fs"
 const env = process.argv[2]
-
-import { readFileSync, writeFileSync } from "fs"
 
 const filepath = "./public/env-config.js"
 const fileCypressPath = "./cypress/env-config.js"
-const data = readFileSync(`./.config-${env}`)
+const data = fs.readFileSync(`./.config-${env}`)
 
 const variables = data.toString()
   .split("\n")
@@ -17,5 +17,5 @@ const variables = data.toString()
   })
   .reduce((res, x) => res.concat(x), "")
 
-writeFileSync(filepath, `window.__ENV__ = { ${variables} }`)
-writeFileSync(fileCypressPath, `window.__ENV__ = { ${variables} }`)
+fs.writeFileSync(filepath, `window.__ENV__ = { ${variables} }`)
+fs.writeFileSync(fileCypressPath, `window.__ENV__ = { ${variables} }`)
